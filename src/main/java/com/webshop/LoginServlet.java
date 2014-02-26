5cd8c2459a0a56620a62af65b2dae12f412e22a9
oleg-panichev <oleg.panichev@ukr.net>
2014-02-27 00:04:43 +0200

    Initial commit

--- src/main/java/com/webshop/LoginServlet.java
+++ src/main/java/com/webshop/LoginServlet.java
@@ -14,8 +14,15 @@ import java.sql.SQLException;
  * Created by Oleg on 25.02.14.
  */
 public class LoginServlet extends javax.servlet.http.HttpServlet {
+    String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         PrintWriter out=response.getWriter();
         String login = request.getParameter("login");
         String pass = request.getParameter("password");
@@ -26,33 +33,37 @@ public class LoginServlet extends javax.servlet.http.HttpServlet {
             out.print("<h2><font color=red>Error!</font><h2></br>Login and password cannot contain \",<,>,' symbols.");
             out.print("</body></html>");
         } else {
-            String workingDir = "D:\\Dropbox\\Java\\JavaWebShop\\db";
+            //String workingDir = "D:\\Dropbox\\Java\\JavaWebShop\\db";
+            String workingDir = "D:/Dropbox/Java/JavaWebShop/db";
             Connection con= null;
             try {
-                con = DriverManager.getConnection("jdbc:derby:"+workingDir);
+                con = DriverManager.getConnection("jdbc:derby://localhost:1527/"+workingDir);
             } catch (SQLException e) {
                 out.print("<html><head><title>Error</title><body>");
-                out.print("<h2><font color=red>Error!</font><h2></br>jdbc:derby:" + workingDir);
+                out.print("<h2><font color=red>Error!</font></h2></br>jdbc:derby:"+workingDir+"</br>");
+                out.print(e.toString());
                 out.print("</body></html>");
-                e.printStackTrace();
+                //e.printStackTrace();
             }
-            UsersDAODb uDao=new UsersDAODb(con);
-            User u=uDao.findUser(login);
-            if (u==null) {
-                if (pass.length()>3) {
-                    uDao.addUser(login,pass);
-                    out.print("<html><head><title>Login</title><body>Login good</body></html>");
-                } else {
+            if (con!=null) {
+                UsersDAODb uDao=new UsersDAODb(con);
+                User u=uDao.findUser(login);
+                if (u==null) {
+                    if (pass.length()>3) {
+                        uDao.addUser(login,pass);
+                        out.print("<html><head><title>Login</title><body>Login good</body></html>");
+                    } else {
 
+                    }
+                } else {
+                    if (u.checkPass(pass))
+                        out.print("<html><head><title>Login</title><body>Login good</body></html>");
                 }
-            } else {
-                if (u.checkPass(pass))
-                    out.print("<html><head><title>Login</title><body>Login good</body></html>");
-            }
-            try {
-                con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
